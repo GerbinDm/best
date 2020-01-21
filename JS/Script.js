dfbd92e6cab19b7985da1016fb4d7c5ef8f0d10d
Alex <gorshkov.sash@gmail.com>
2020-01-21 16:53:31 +0200

    23tasks
    js content

--- JS/Script.js
+++ JS/Script.js
@@ -47,19 +47,6 @@ function FifthTask(){
     o=(n % 2 == 0)
     alert(o)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 function sixthTask (){
     var a=prompt('input  1  number'),d;
     var b=prompt('input  2  number');
@@ -69,21 +56,251 @@ function sixthTask (){
     alert('max is '+d);
 
 }
-
-function eighthTask(){
+function seventhTask(){
     var input=prompt('Ill check if your simbol is number'),o;
     input=+input;
     o=((input+input)==(input*2))
     alert(o);
 }
-
-function ninthTask(){
+function eighthTask(){
     var p=Math.round(Math.random()*100);
     var t=prompt('input number'),d;
     d=Math.max(p,t)
     alert('max is '+d +' randomized is '+ p +' inputed '+ t)
 }
-function tenthTask(){
+function ninthTask(){
     var p, t;
     alert ("x < y = " + ((p = Math.round(Math.random()*100)) < (t = prompt('Input your number', ''))) + ", x = " + p + ", y = " + t);
 }
+function tenthTask() {
+    var x=+prompt("Input number 1"),y=+prompt("Input number 2"),z=+prompt("Input number 3"),m;
+    m=+Math.min(x,y,z)
+    if (x==y || x==z || y==z) {
+
+        alert("there are several same and the min = " +m)}
+    else
+        switch (m) {
+            case (x ):
+                alert("x is the min = "+x)	;
+                break;
+            case (y ):
+                alert("y is min = " +y);
+                break;
+            case (z ):
+                alert("z is min = "+z);
+                break;
+            default:
+                alert("there are several same and the min  = " +m)
+        }
+
+}
+function elevenTask() {
+    var x=prompt("Input number");
+    if (x < 0)
+        alert("x is negative = "+x)	;
+    else if (x == 0)
+        alert("x is zero")	;
+    else
+        alert("x is positive = "+x)	;
+
+}
+function twelveTask() {
+    var my=+prompt("your money"),his=+prompt("friends money"),tour=+prompt("money need to go to Mayorka");
+    if (my+his < tour)
+        alert("relax and go to dreenk beer ")	;
+    else
+        alert("go to mayorka ")	;
+}
+function thrtnTask() {
+    var age=+prompt("input age");
+    if (age >= 20 && age < 27)
+        alert("send invite to NAVY ")	;
+    else
+        alert("relax ")	;
+}
+function frtnTask() {
+    var num=+prompt("input number of taxi");
+    switch (num) {
+        case (7):alert("go home");break;
+        case (225):alert("go home" );break;
+        case (255):alert("go home");break;
+        default:alert("wait");
+    }
+}
+function fftnTask(){
+    var date = new Date();
+    var day =date.getDay();
+    if (day <= 5) {
+        alert("go to the work");
+    } else {
+        alert("have a rest");
+    }
+}
+function sxtnTask (){
+    var x=+prompt("input number x");
+    var y=+prompt("input number y");
+    if (x <= 1 && y>=3 || y<0 ) {
+        var sum=x+y
+        alert("sum= "+sum);
+    }
+    else {
+        alert("incorrect");
+    }
+}
+function svtnTask (){
+    var x=+prompt("input number x");
+    var y=+prompt("input number y");
+    if (x <= 1 && y>=3 || y<0 ) {
+        var sum=x+y
+        alert("sum= "+sum);
+    }
+    else {
+        alert("incorrect");
+    }
+}
+function eghtnTask(){
+    var x=+prompt("input number x");
+    var y=+prompt("input number y");
+    if (x > 2 && x < 11 || y >= 6 && y < 14 ) {
+        var sum=x+2
+        alert("sum= "+sum);
+    }
+    else {
+        var sum=x+5
+        alert("sum= "+sum);
+
+    }
+}
+function nntnTask(){
+    var name=prompt("input name");
+    if(name==null){
+        alert("cancel");
+    } else if(name==""){
+        alert("empty field");
+    } else
+        alert("Hello "+name);
+}
+function twlwTask(){
+    var greeting="";
+    var lan=prompt("input language (ru,en,de)");
+    switch (lan) {
+        case "ru":greeting="Привет";alert(greeting);break;
+        case "en":greeting="Hello";alert(greeting);break;
+        case "de":greeting="Hallo";alert(greeting);break;
+        default:alert("wrong language");
+    }
+}
+function twntoTask() {
+    var date = new Date();
+    var month = date.getMonth();
+    month = month + 1;
+    alert("month is " + month);
+    if (month <=2 || month == 12) {
+        alert("winter");
+    } else if (month <= 5 ) {
+        alert("spring");
+    } else if ( month <= 8) {
+        alert("summer");
+    } else {
+        alert("autumn");
+    }
+}
+function twnttoask() {
+    var lang = prompt("input language(en,ru)");
+    var day = new Date().getDay();
+    var curDay;
+    if (lang == "ru") {
+        switch (day) {
+            case 0:
+                curDay = "Воскресенье";
+                break;
+            case 1:
+                curDay = "Понедельник";
+                break;
+            case 2:
+                curDay = "Вторник";
+                break;
+            case 3:
+                curDay = "Среда";
+                break;
+            case 4:
+                curDay = "Четверг";
+                break;
+            case 5:
+                curDay = "Пятница";
+                break;
+            case 6:
+                curDay = "Суббота";
+                break;
+        }
+        alert("Сегодня " + curDay);
+    } else if (lang == "en") {
+        switch (day) {
+            case 0:
+                curDay = "Sunday";
+                break;
+            case 1:
+                curDay = "Monday";
+                break;
+            case 2:
+                curDay = "Tusday";
+                break;
+            case 3:
+                curDay = "Wednesday";
+                break;
+            case 4:
+                curDay = "Thursday";
+                break;
+            case 5:
+                curDay = "Friday";
+                break;
+            case 6:
+                curDay = "Saturday";
+                break;
+        }
+        alert("Today is " + curDay);
+    } else {
+        alert("wrong language")
+    }
+
+}
+function twntthrtask() {
+    var x = +prompt("Input number 1"), y = +prompt("Input number 2"), z = +prompt("Input number 3"), m;
+    m = +Math.min(x, y, z)
+    if (x == y || x == z || y == z) {
+
+        alert("there are several same and the min = " + m)
+    } else
+        switch (m) {
+            case (x):
+                alert("x is the min = " + x);
+                break;
+            case (y):
+                alert("y is min = " + y);
+                break;
+            case (z):
+                alert("z is min = " + z);
+                break;
+            default:
+                alert("there are several same and the min  = " + m)
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
