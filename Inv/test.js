0adf4423792be8e5671210d4663492b240fb802e
GerbinDm <42175450+GerbinDm@users.noreply.github.com>
2020-02-05 18:24:59 +0200

    JS
    zadachi v inv 6

--- Inv/test.js
+++ Inv/test.js
@@ -1,91 +1,117 @@
 function newtask() {
-    var achar,bchar,cchar;
-    achar = prompt ('input value1 ' );
-    bchar = prompt ('input value2 ' );
-    cchar = prompt ('input value3 ' );
+    var achar, bchar, cchar;
+    achar = prompt('input value1 ');
+    bchar = prompt('input value2 ');
+    cchar = prompt('input value3 ');
 
-    achar=+achar;
-    bchar=+bchar;
-    cchar=+cchar;
+    achar = +achar;
+    bchar = +bchar;
+    cchar = +cchar;
 
-    console.log(achar,bchar,cchar);
-    alert( +achar+" "+bchar+" "+cchar);
+    console.log(achar, bchar, cchar);
+    alert(+achar + " " + bchar + " " + cchar);
 
     var summ;
 
 
-    summ=+achar+bchar+cchar;
+    summ = +achar + bchar + cchar;
 
     console.log(summ);
     alert(+summ);
 
     var avg;
 
-    avg=(achar+bchar+cchar)/3;
+    avg = (achar + bchar + cchar) / 3;
 
     console.log(avg);
-    alert('srednee   ' +avg);
+    alert('srednee   ' + avg);
 
-    console.log('max' +Math.max(achar, bchar, cchar));
-    alert   ('Max '+Math.max(achar, bchar, cchar));
- var minim = +Math.min(achar, bchar, cchar);
-    console.log('min' +Math.min(achar, bchar, cchar));
-    alert   ('Min '+Math.min(achar, bchar, cchar));
+    console.log('max' + Math.max(achar, bchar, cchar));
+    alert('Max ' + Math.max(achar, bchar, cchar));
+    var minim = +Math.min(achar, bchar, cchar);
+    console.log('min' + Math.min(achar, bchar, cchar));
+    alert('Min ' + Math.min(achar, bchar, cchar));
 
     var summ2;
 
-    summ2=+ summ*100/50;
+    summ2 = +summ * 100 / 50;
 
-    console.log('summa umnozit na 100 padelit na 50        ' +summ2);
-    alert   ('summ x 100_ /_na_50 ravno =        ' +summ2);
+    console.log('summa umnozit na 100 padelit na 50        ' + summ2);
+    alert('summ x 100_ /_na_50 ravno =        ' + summ2);
 
     var pr;
 
-    pr=(+achar*bchar*cchar);
+    pr = (+achar * bchar * cchar);
 
-    console.log('proizvedenie       ' +pr);
-    alert   ('proizvedenie =        ' +pr);
+    console.log('proizvedenie       ' + pr);
+    alert('proizvedenie =        ' + pr);
 
 
     var c;
     var d;
 
-    c=+summ/2;
-    d=Math.round(c);
+    c = +summ / 2;
+    d = Math.round(c);
 
-    console.log('proverka na chet/nechet          ' +c+d);
-    if (c==d){
-        alert('chet           '    )
-}
-
-    else{
-        alert('nechet       '     )
+    console.log('proverka na chet/nechet          ' + c + d);
+    if (c == d) {
+        alert('chet           ')
+    } else {
+        alert('nechet       ')
     }
 
 
-    if (achar==achar/2*2 && bchar==bchar/2*2 && cchar==cchar/2*2){
+    if (achar == achar / 2 * 2 && bchar == bchar / 2 * 2 && cchar == cchar / 2 * 2) {
 
-            alert('cifra')
+        alert('cifra')
 
-        }
-
-    else
+    } else
         alert('bukva')
 
     let str = summ;
 
     for (let i = 0; i <= 4; i++) {
-        str = str*2;
+        str = str * 2;
         console.log(str);
         alert(str)
     }
-    for (let i = minim; i>=0; i--) {
+    for (let i = minim; i > 0; i--) {
 
 
-        console.log('i',i);
+        console.log('i', i);
     }
 
-
 }
+
+
+
+
+
+
+function wtf() {
+    let sum = 0;
+    let arr = [];
+    while (true) {
+        let value = +prompt("Введите число", '');
+        console.log(value);
+        if (value){
+            arr.push(value);
+        console.log(arr);}
+        else if(isNaN(value))
+            alert('NICHO');
+        else
+        break;
+
+    }
+    alert( 'Сумма: ' + sum );
+    alert(arr);
+}
+
+
+
+
+
+
+
 
 
